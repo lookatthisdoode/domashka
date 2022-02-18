970ffaf3405edf5a1ba527229beb2f84d2dc9b57
lookatthisdoode <andruhahoke@gmail.com>
2022-02-18 21:50:41 +0200

    added sum code

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,2 +1,16 @@
-echo Hello World
+//echo Hello World
+#include <iostream>
+using namespace std;
+int main {
 cout << "hello" << endl;
+
+int choose;
+cin >> choose;
+if (choose=1)
+	cout << "helloworld";
+
+
+return 0;
+
+
+
